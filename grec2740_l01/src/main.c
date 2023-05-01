7a0d6dcc5035cacf14d783cdfd24590d89f695e4
Andrew Greco <107579695+grecs08@users.noreply.github.com>
2023-05-01 13:38:57 -0400

    Add files via upload

---
+++ grec2740_l01/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int number = 0;
+	printf("Enter a number: ");
+	scanf("%d", &number);
+	printf("The number you entered is %d\n", number);
+	return 0;
+}
